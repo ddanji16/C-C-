dd8c9a35c2259311584b6ed51963a662bccb9b06
Daniel anthony Dela cruz <anthonydelac1602@gmail.com>
2025-05-01 19:42:17 +0800

    new files

---
+++ index.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+  cout << "Hello worlds" << endl;
+
+
+
+/*
+//Function adding two numbers 
+
+float sum(float num1, float num2) {
+
+    return num1 + num2;
+
+}
+
+
+int main() {
+
+   float num1;
+   float num2;
+ 
+   cout << "Enter first number: ";
+   cin >> num1;
+   
+   cout << "Enter second Number: ";
+   cin >> num2;
+
+    cout << "The Sum of two numbers is : " << sum(num1,num2);
+    
+
+
+*/
+
+
+
+
+
+
+
+/*
+
+// FUNCTIONS
+
+
+int sum(int num1, int num2) {
+
+    return num1 + num2;
+
+}
+
+void greatings() {
+
+    cout << "Hello, Welcome";
+
+}
+
+int main() {
+
+
+
+    cout << sum(10,20) <<endl;
+
+    greatings();
+
+
+
+
+
+ */
+
+
+/*
+
+    // ARRRAYS
+
+
+
+    int numbers[10];
+    numbers [4] = 100;
+
+
+    cout << "Enter first Number: ";
+    cin >> numbers[0];
+
+    cout << "Enter Second Number: ";
+    cin >> numbers[1];
+
+    cout << "Enter Third Number: ";
+    cin >> numbers[2];
+
+    cout << "Enter fourth Number: ";
+    cin >> numbers[3];
+
+
+
+
+    cout << "The sum of all numbers is: " << numbers [0] + numbers [1] + numbers [2] + numbers [3] + numbers [4];
+
+
+
+*/
+
+
+
+
+/*
+
+   //OUTPUT & INPUT
+
+ int num1;
+ int num2;
+
+
+ cout<<"Enter first number: ";
+ cin >>num1;
+
+ cout <<"Enter Second Number: ";
+ cin >>num2;
+
+ cout << "The sum is: " << num1 + num2;
+*/
+return 0;
+}
